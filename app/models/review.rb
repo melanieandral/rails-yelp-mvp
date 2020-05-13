be560f9f3821fe11fadd5827a9856b21aab14750
Melanie Andral <m.andral@montpellier-bs.com>
2020-05-13 15:16:22 +0200

    Controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurants
-  validates :content, presence: true
-  validates :rating, presence: true
-  validates :rating, inclusion: { in: 0..5 }
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true}
 end
